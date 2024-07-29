997d3801822191a9230ac61d70a55be3a5614ab3
julian <julian.lemmel@tuwien.ac.at>
2024-07-29 17:44:12 +0900

    dsae dev

--- models/dsae.py
+++ models/dsae.py
@@ -1,11 +1,19 @@
 """Deep Spatial Auto-Encoder using flax.
 
-Taken from https://github.com/gorosgobe/dsae-torch/tree/master"""
+Taken from https://github.com/gorosgobe/dsae-torch/tree/master
 
-from dataclasses import dataclass
+References:
+    [1]: "Deep Spatial Autoencoders for Visuomotor Learning"
+    Chelsea Finn, Xin Yu Tan, Yan Duan, Trevor Darrell, Sergey Levine, Pieter Abbeel
+    Available at: https://arxiv.org/pdf/1509.06113.pdf
+    [2]: https://github.com/tensorflow/tensorflow/issues/6271
+"""
+
+from dataclasses import dataclass, field
 import jax.numpy as jnp
 from flax import linen as nn
 from jax.nn import softmax
+import numpy as np
 
 
 def get_image_coordinates(h, w, normalise):
@@ -20,22 +28,35 @@ def get_image_coordinates(h, w, normalise):
 
 
 class SpatialSoftArgmax(nn.Module):
+    """Applies a spatial soft argmax over the input images.
+    :param temperature: The temperature parameter (float). If None, it is learnt.
+    :param normalise: Should spatial features be normalised to range [-1, 1]?
+    """
+
     temperature: float = None
     normalise: bool = False
 
-    def setup(self):
-        self.temperature = self.param(
-            "temperature", lambda rng: jnp.ones(1) if self.temperature is None else jnp.array([self.temperature])
-        )
-
+    @nn.compact
     def __call__(self, x):
-        n, c, h, w = x.shape
-        spatial_softmax_per_map = softmax(x.reshape(n * c, h * w) / self.temperature, axis=-1)
-        spatial_softmax = spatial_softmax_per_map.reshape(n, c, h, w)
+        """Apply Spatial SoftArgmax operation on the input batch of images x.
+        :param x: batch of images, of size (N, C, H, W)
+        :return: Spatial features (one point per channel), of size (N, C, 2)
+        """
+        c, h, w = x.shape[-3:]
+        _temperature = (
+            self.param("temperature", lambda _: jnp.ones(1))
+            if self.temperature is None
+            else jnp.array([self.temperature])
+        )
+        spatial_softmax_per_map = softmax(x.reshape(-1, h * w) / _temperature, axis=-1)
+        spatial_softmax = spatial_softmax_per_map.reshape(-1, c, h, w).squeeze()
 
+        # calculate image coordinate maps
         image_x, image_y = get_image_coordinates(h, w, normalise=self.normalise)
+        # size (H, W, 2)
         image_coordinates = jnp.stack((image_x, image_y), axis=-1)
 
+        # multiply coordinates by the softmax and sum over height and width, like in [2]
         expanded_spatial_softmax = spatial_softmax[..., None]
         image_coordinates = image_coordinates[None, None, ...]
         out = jnp.sum(expanded_spatial_softmax * image_coordinates, axis=(2, 3))
@@ -43,77 +64,109 @@ class SpatialSoftArgmax(nn.Module):
 
 
 class DSAE_Encoder(nn.Module):
-    in_channels: int
+    """Creates a Deep Spatial Autoencoder encoder
+    :param out_channels: Output channels for each of the layers. The last output channel corresponds to half the
+    size of the low-dimensional latent representation.
+    :param temperature: Temperature for spatial soft argmax operation. See SpatialSoftArgmax.
+    :param normalise: Normalisation of spatial features. See SpatialSoftArgmax.
+    """
+
     out_channels: tuple
     temperature: float = None
     normalise: bool = False
 
-    def setup(self, train: bool = True):
-        self.conv1 = nn.Conv(features=self.out_channels[0], kernel_size=(7, 7), strides=(2, 2))
-        self.batch_norm1 = nn.BatchNorm(use_running_average=not train)
-        self.conv2 = nn.Conv(features=self.out_channels[1], kernel_size=(5, 5))
-        self.batch_norm2 = nn.BatchNorm(use_running_average=not train)
-        self.conv3 = nn.Conv(features=self.out_channels[2], kernel_size=(5, 5))
-        self.batch_norm3 = nn.BatchNorm(use_running_average=not train)
-        self.spatial_soft_argmax = SpatialSoftArgmax(temperature=self.temperature, normalise=self.normalise)
-
-    def __call__(self, x):
-        out_conv1 = nn.relu(self.batch_norm1(self.conv1(x)))
-        out_conv2 = nn.relu(self.batch_norm2(self.conv2(out_conv1)))
-        out_conv3 = nn.relu(self.batch_norm3(self.conv3(out_conv2)))
-        out = self.spatial_soft_argmax(out_conv3)
+    @nn.compact
+    def __call__(self, x, train: bool = True):
+        x = nn.Conv(features=self.out_channels[0], kernel_size=(7, 7), strides=(2, 2))(x)
+        x = nn.relu(nn.BatchNorm()(x, use_running_average=not train))
+        x = nn.Conv(features=self.out_channels[1], kernel_size=(5, 5))(x)
+        x = nn.relu(nn.BatchNorm()(x, use_running_average=not train))
+        x = nn.Conv(features=self.out_channels[2], kernel_size=(5, 5))(x)
+        x = nn.relu(nn.BatchNorm()(x, use_running_average=not train))
+        out = SpatialSoftArgmax(temperature=self.temperature, normalise=self.normalise)(x)
         return out
 
 
 class DSAE_Decoder(nn.Module):
+    """
+    Creates a Deep Spatial Autoencoder decoder
+    :param image_output_size: (height, width) of the output, grayscale image
+    :param normalise: True if output in range [-1, 1], False for range [0, 1]
+    """
+
     image_output_size: tuple
     normalise: bool = True
 
-    def setup(self):
-        self.channels, self.height, self.width = self.image_output_size
-        self.decoder = nn.Dense(features=self.channels * self.height * self.width)
-        self.activ = nn.tanh if self.normalise else nn.sigmoid
-
+    @nn.compact
     def __call__(self, x):
-        out = self.activ(self.decoder(x))
-        out = out.reshape(-1, self.channels, self.height, self.width)
+        activ = nn.tanh if self.normalise else nn.sigmoid
+        out = activ(nn.Dense(features=np.prod(self.image_output_size))(x))
+        out = out.reshape(*self.image_output_size)
         return out
+
 
 @dataclass
 class DSAEConfig:
+    """
+    :param image_output_size: Reconstructed image size
+    :param in_channels: Number of channels of input image
+    :param out_channels: Output channels of each conv layer in the encoder.
+    :param latent_dimension: Input dimension for decoder
+    :param temperature: Temperature parameter, None if it is to be learnt
+    :param normalise: Should spatial features be normalised to [-1, 1]?
+    """
+
     channels: tuple = (64, 32, 16)
-    latent_dimension: int = 32
-    temperature: float = None
-    normalise: bool = False
-    
-class DeepSpatialAutoencoder(nn.Module):
-    image_output_size: tuple = (60, 60)
-    in_channels: int = 3
-    channels: tuple = (64, 32, 16)
-    latent_dimension: int = 32
     temperature: float = None
     normalise: bool = False
 
+
+class DeepSpatialAutoencoder(nn.Module):
+    """A deep spatial autoencoder. Default parameters are the ones used in [1], with the original input image
+    being 3x240x240. See docs for encoder and decoder.
+
+    """
+
+    image_output_size: tuple
+    config: DSAEConfig = field(default_factory=DSAEConfig)
+
     def setup(self):
-        if self.channels[-1] * 2 != self.latent_dimension:
-            raise ValueError("Spatial SoftArgmax produces a location (x,y) per feature map!")
         self.encoder = DSAE_Encoder(
-            in_channels=self.in_channels,
-            out_channels=self.channels,
-            temperature=self.temperature,
-            normalise=self.normalise,
+            out_channels=self.config.channels,
+            temperature=self.config.temperature,
+            normalise=self.config.normalise,
         )
         self.decoder = DSAE_Decoder(image_output_size=self.image_output_size)
 
-    def __call__(self, x):
-        spatial_features = self.encoder(x)
+    def encode(self, x, train: bool = True):
+        """Encode given Image."""
+        return self.encoder(x, train=train)
+
+    def decode(self, latent):
+        """Decode Image from latent vector."""
+        return self.decoder(latent)
+
+    def __call__(self, x, train: bool = True):
+        spatial_features = self.encoder(x, train=train)
         n, c, _2 = spatial_features.shape
-        return self.decoder(spatial_features.reshape(n, c * 2))
+        return self.decoder(spatial_features.reshape(n, c * 2)), spatial_features
 
 
 def dsae_loss(reconstructed, target, ft_minus1=None, ft=None, ft_plus1=None):
+    """Compute Loss for deep spatial autoencoder.
+    For the start of a trajectory, where ft_minus1 = ft, simply pass in ft_minus1=ft, ft=ft
+    For the end of a trajectory, where ft_plus1 = ft, simply pass in ft=ft, ft_plus1=ft
+    :param reconstructed: Reconstructed, grayscale image
+    :param target: Target, grayscale image
+    :param ft_minus1: Features produced by the encoder for the previous image in the trajectory to the target one
+    :param ft: Features produced by the encoder for the target image
+    :param ft_plus1: Features produced by the encoder for the next image in the trajectory to the target one
+    :return: A tuple (mse, g_slow) where mse = the MSE reconstruction loss and g_slow = g_slow contribution term ([1])
+    """
     mse_loss = jnp.sum((reconstructed - target) ** 2)
     g_slow_contrib = 0.0
+    loss_info = {"reconstruction_loss": mse_loss}
     if ft_minus1 is not None:
         g_slow_contrib = jnp.sum((ft_plus1 - ft - (ft - ft_minus1)) ** 2)
-    return mse_loss, g_slow_contrib
+        loss_info["g_slow":g_slow_contrib]
+    return mse_loss + g_slow_contrib, loss_info
