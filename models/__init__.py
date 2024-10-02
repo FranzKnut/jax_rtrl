91e440abe58cf0c1922a10dd29d320d8e12b2dd5
Julian <julian.lemmel@tuwien.ac.at>
2024-10-02 14:18:08 +0200

    init_model utility function

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
+import flax
+import flax.linen
+import jax
 from jax_rtrl.models.lru.lru_bptt import LRULayer
 from jax_rtrl.models.lru.online_lru import OnlineLRUCell, OnlineLRULayer
 from .ctrnn.ctrnn import CTRNNCell, OnlineCTRNNCell
@@ -9,5 +12,13 @@ CELL_TYPES = {
     "rflo": OnlineCTRNNCell,
     "rtrl": OnlineCTRNNCell,
     "lru": LRULayer,
-    "lru_rtrl": OnlineLRULayer
+    "lru_rtrl": OnlineLRULayer,
 }
+
+
+def init_model(model: flax.linen.Module, sample_input, is_rnn: bool, rng_key=None):
+    rng_key = rng_key or jax.random.PRNGKey(0)
+    if is_rnn:
+        carry = model.initialize_carry(rng_key, sample_input.shape)
+        return model.init(rng_key, carry, sample_input)
+    return model.init(rng_key, sample_input)
